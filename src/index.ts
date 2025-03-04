cbb7e5b131fcf5ed602f9feca64ef4dbe87bb1d8
Astro <maxsimilijano@gmail.com>
2025-03-05 00:29:54 +0100

    chore: object oriented programming

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,55 @@
-import { Client, GatewayIntentBits, IntentsBitField, Message } from "discord.js";
+import { Client, IntentsBitField, Message, REST } from "discord.js";
 import fs from 'fs';
 import toml from 'toml';
 
-const bot_client = new Client({ intents: [ GatewayIntentBits.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent ] });
+class DBot extends Client {
+    kogBot: KOGBot;
+    REST: REST = new REST();
 
-const toml_file = fs.readFileSync('config.toml', 'utf8').toString();
-const env = toml.parse(toml_file);
+    constructor (kogBot: KOGBot) {
+        super({
+            intents: [
+                IntentsBitField.Flags.Guilds,
+                IntentsBitField.Flags.GuildMessages,
+                IntentsBitField.Flags.MessageContent
+            ]
+        });
+        this.kogBot = kogBot;
 
-bot_client.on("ready", () => {
-    console.log("Bot is ready");
-});
+        this.REST.setToken(this.kogBot.environment.discord.client_token);
 
-bot_client.on("messageCreate", (message: Message) => {
-    if (message.content.includes("stinky")) {
-        message.reply("mewow mrrp")
+        if (!kogBot.ci_workflow) {
+            this.login(this.kogBot.environment.discord.client_token);
+
+            this.on('ready', () => {
+                console.log(`Logged in as ${this.user?.tag}`);
+            });
+
+            this.on('messageCreate', (message: Message) => {
+                if (message.content === 'ping') {
+                    message.reply('pong \:D');
+                }
+            });
+        }
     }
-});
+}
 
-bot_client.login(env.discord.client_token);
+class KOGBot_Client {
+    environment;
+    ci_workflow: boolean = false;
+    discord_client: DBot;
+
+    constructor () {
+        const toml_file = fs.readFileSync('config.toml', 'utf8').toString();
+        this.environment = toml.parse(toml_file);
+
+        this.discord_client = new DBot(this);
+
+    }
+}
+
+const KOGBot = new KOGBot_Client();
+export default KOGBot;
+export type KOGBot = typeof KOGBot;
 
 // this is temporary so STUPID SHEEP CAN HAVE FUN.
