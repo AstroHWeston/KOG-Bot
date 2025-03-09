85aa0e90422f191b20da3a9bc0be0fad2691064f
Sheepboi546KOG <sheepatkog546@outlook.com>
2025-03-09 23:00:49 +0000

    event schedule command voted by MR

---
+++ src/commands/main/event.ts
@@ -0,0 +1,8 @@
+// user runs command
+// model created
+// submitted and is put in event schedule
+// UUIDv4 thing created
+// stored in DB
+// subcommmand of start which requires UUID token
+// - pings rookies and insurgancy
+// add cancel and stuff
