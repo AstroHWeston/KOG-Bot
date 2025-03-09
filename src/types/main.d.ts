0ac147339cf2d6bb6b2b37660a0373d808d3b061
Astro <maxsimilijano@gmail.com>
2025-03-09 11:46:34 +0100

    chore: Add slash command interface

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,11 +1,25 @@
+import { ApplicationCommandOptionBase, ChatInputCommandInteraction } from "discord.js";
+import { KOGBot } from "index.ts";
+
 declare interface GatewayEvent {
     name: string,
-    code: (...params) => Promise<void>;
     once: boolean;
+    code: (...params) => Promise<void>;
 }
 
 declare interface EventsClass {
     functions: Array<GatewayEvent>;
     parse: () => Promise<GatewayEvent[]>;
     listen: () => Promise<void>;
+}
+
+declare interface SlashCommand {
+    name: string;
+    description: string;
+    subcommands: Array<SlashCommand>;
+    parameters: Array<ApplicationCommandOptionBase>;
+    dev?: boolean;
+    kogBot: KOGBot;
+    
+    execute (interaction: ChatInputCommandInteraction): Promise<void>;
 }
