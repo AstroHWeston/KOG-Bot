7b7312db9a08c6427c23af5e70139bad66306707
Astro <maxsimilijano@gmail.com>
2025-03-09 11:54:25 +0100

    fuck u

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,25 +1,27 @@
 import { ApplicationCommandOptionBase, ChatInputCommandInteraction } from "discord.js";
 import { KOGBot } from "index.ts";
 
-declare interface GatewayEvent {
-    name: string,
-    once: boolean;
-    code: (...params) => Promise<void>;
-}
-
-declare interface EventsClass {
-    functions: Array<GatewayEvent>;
-    parse: () => Promise<GatewayEvent[]>;
-    listen: () => Promise<void>;
-}
-
-declare interface SlashCommand {
-    name: string;
-    description: string;
-    subcommands: Array<SlashCommand>;
-    parameters: Array<ApplicationCommandOptionBase>;
-    dev?: boolean;
-    kogBot: KOGBot;
+global {
+    declare interface GatewayEvent {
+        name: string,
+        once: boolean;
+        code: (...params) => Promise<void>;
+    }
     
-    execute (interaction: ChatInputCommandInteraction): Promise<void>;
+    declare interface EventsClass {
+        functions: Array<GatewayEvent>;
+        parse: () => Promise<GatewayEvent[]>;
+        listen: () => Promise<void>;
+    }
+    
+    declare interface SlashCommand {
+        name: string;
+        description: string;
+        subcommands: Array<SlashCommand>;
+        parameters: Array<ApplicationCommandOptionBase>;
+        dev?: boolean;
+        kogBot: KOGBot;
+        
+        execute (interaction: ChatInputCommandInteraction): Promise<void>;
+    }
 }
