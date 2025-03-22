ad589568461ceed9690c9f174b97335c89d2a344
Astro <maxsimilijano@gmail.com>
2025-03-22 15:36:53 +0100

    feat: Event log command.

--- src/commands/main/log.ts
+++ src/commands/main/log.ts
@@ -2,14 +2,18 @@ import {
     EmbedBuilder,
     ChatInputCommandInteraction,
     TextChannel,
-    DMChannel,
-    NewsChannel,
     Colors,
     SlashCommandBuilder,
     ActionRowBuilder,
     ButtonBuilder,
     ButtonStyle,
-    GuildMemberRoleManager
+    UserSelectMenuBuilder,
+    ComponentType,
+    MessageFlags,
+    ButtonInteraction,
+    CollectorFilter,
+    Interaction,
+    Channel
 } from 'discord.js';
 import { KOGBot } from '../../index.js';
 
@@ -18,7 +22,8 @@ class LogEventCommand implements SlashCommand {
         .setName('logevent')
         .setDescription('Log an event.');
 
-    dev = false; // reverting after kapatz testing g
+    dev = false;
+    mr = true;
     kogBot: KOGBot;
 
     constructor(kogBot: KOGBot) {
@@ -26,151 +31,222 @@ class LogEventCommand implements SlashCommand {
     }
 
     async execute(interaction: ChatInputCommandInteraction): Promise<void> {
-        
-        const db = this.kogBot.database;
+        let loggedUsers: string[] = [];
 
-        const member = interaction.member;
+        const userSelect = new UserSelectMenuBuilder()
+            .setCustomId('attendee_list')
+            .setPlaceholder('Select event attendees.')
+            .setMinValues(1)
+            .setMaxValues(25);
 
-        const mrRoleId = this.kogBot.environment.discord.mr_role;
+        const finalizeButton = new ButtonBuilder()
+            .setCustomId('finalize_log')
+            .setLabel('Finalize')
+            .setStyle(ButtonStyle.Success)
+            .setEmoji('✅');
 
-        if (!member || !('roles' in member) || !(member.roles instanceof GuildMemberRoleManager) || !member.roles.cache.has(mrRoleId)) {
-            const errorEmbed = new EmbedBuilder()
-            .setColor(Colors.Red)
-            .setTitle('Access Denied')
-            .setDescription('You do not have the required role to use this command.')
-            .setTimestamp();
+        const cancelButton = new ButtonBuilder()
+            .setCustomId('cancel_log')
+            .setLabel('Cancel')
+            .setStyle(ButtonStyle.Danger)
+            .setEmoji('❌');
 
-            await interaction.reply({
-            embeds: [errorEmbed]
-            });
-            return;
-        }
+        const buttonRow = new ActionRowBuilder<ButtonBuilder>()
+            .addComponents(finalizeButton, cancelButton);
+
+        const selectionRow = new ActionRowBuilder<UserSelectMenuBuilder>()
+            .addComponents(userSelect);
 
         const initialEmbed = new EmbedBuilder()
             .setColor(Colors.Yellow)
-            .setTitle('Logging Event')
-            .setDescription('Please mention all attendees\n\n**Example:** <@1234567890>, <@0987654321>, <@1234567890>\n\nPlease follow the format above if you can.')
-            .setTimestamp();
+            .setTitle('Event log')
+            .setDescription('Hello! Thank you for starting an event log. Please use the action row below and select all attendees that have attended the event. You can mention multiple users in one go.')
+            .setFields(
+                { name: 'Host', value: `<@${interaction.user.id}>` },
+                { name: 'Attendees', value: 'None.' }
+            )
+            .setTimestamp()
 
-        await interaction.reply({ embeds: [initialEmbed] });
 
-        const messageFilter = (response: { author: { id: string } }) => response.author.id === interaction.user.id;
+        const res = await interaction.reply({
+            embeds: [ initialEmbed ],
+            components: [ selectionRow, buttonRow ],
+            withResponse: true
+        });
 
-        if (interaction.channel instanceof TextChannel || interaction.channel instanceof DMChannel || interaction.channel instanceof NewsChannel) {
-            const messageCollected = await interaction.channel.awaitMessages({
-                filter: messageFilter,
-                max: 1,
-                time: 60_000,
-            }).catch(() => null);
+        const collector = res.resource?.message?.createMessageComponentCollector({
+            componentType: ComponentType.UserSelect
+        });
 
-            const response = messageCollected?.first()?.content;
-
-            if (!response) {
-                const timeoutEmbed = new EmbedBuilder()
-                    .setColor(Colors.Red)
-                    .setTitle('Timeout')
-                    .setDescription('You took too long to respond. Please try again.')
-                    .setTimestamp();
-
-                await interaction.followUp({ embeds: [timeoutEmbed] });
+        collector?.on('collect', async (i) => {
+            if (i.user.id !== interaction.user.id) {
+                i.reply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle('Unauthorized')
+                            .setDescription('You are not authorized to interact with this message.\n Only the initial user can interact with this message.')
+                            .setColor(Colors.Red)
+                            .setTimestamp()
+                            .setFooter({
+                                text: 'KOG Bot',
+                                iconURL: interaction.guild?.iconURL() as string
+                            })
+                    ],
+                    flags: MessageFlags.Ephemeral
+                })
                 return;
             }
+            const selection = i.values;
+            loggedUsers = selection;
 
-            const mentionRegex = /<@!?(\d+)>/g;
-            const mentions = Array.from(response.matchAll(mentionRegex), m => m[0]);
+            await i.update({
+                embeds: [
+                    initialEmbed.setFields(
+                        { name: 'Host', value: `<@${interaction.user.id}>` },
+                        { name: 'Attendees', value: loggedUsers.map(user => `- <@${user}>`).join('\n') }
+                    )
+                ]
+            });
+        });
 
-            if (mentions.length === 0) {
-                const errorEmbed = new EmbedBuilder()
-                    .setColor(Colors.Red)
-                    .setTitle('Error: No Mentions Found')
-                    .setDescription('Please ensure that you mention at least one user.')
-                    .setTimestamp();
+        collector?.on('end', async (collected, reason: string) => {
+            if (reason === 'time') {
+                const disabledRow = new ActionRowBuilder<UserSelectMenuBuilder>()
+                    .addComponents(userSelect.setDisabled(true));
 
-                await interaction.followUp({ embeds: [errorEmbed] });
-                return;
+                await res.resource?.message?.edit({
+                    embeds: [ initialEmbed.setColor(Colors.Red) ],
+                    components: [ disabledRow ]
+                });
             }
+        });
 
-            const successEmbed = new EmbedBuilder()
-                .setColor(Colors.Green)
-                .setTitle('Mentions Collected')
-                .setDescription(
-                    `The following attendees were collected successfully:\n${mentions.join(', ')}\n\nClick **Done** to proceed or **Cancel** to stop.`
-                )
-                .setTimestamp();
+        try {
+            const collectorFilter = (i: ButtonInteraction) => i.user.id === interaction.user.id;
+            const confirmation = await res.resource?.message?.awaitMessageComponent({ filter: collectorFilter, time: 3_600_000, componentType: ComponentType.Button });
 
-            const actionRow = new ActionRowBuilder<ButtonBuilder>()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId('done')
-                        .setLabel('Done')
-                        .setStyle(ButtonStyle.Success),
-                    new ButtonBuilder()
-                        .setCustomId('cancel')
-                        .setLabel('Cancel')
-                        .setStyle(ButtonStyle.Danger)
-                );
+            if (confirmation?.customId === 'finalize_log') {
+                const logChannel = await this.kogBot.discord_client.channels.cache.get(this.kogBot.environment.discord.ids.log_channel_id) as TextChannel;
 
-            await interaction.followUp({ embeds: [successEmbed], components: [actionRow] });
+                if (!logChannel || !logChannel.isTextBased()) {
+                    await confirmation.update({
+                        content: 'The log channel has not been found, please contact the bot developers.',
+                        embeds: [],
+                        components: []
+                    });
+                    return;
+                }
 
-            const buttonFilter = (i: { user: { id: string } }) => i.user.id === interaction.user.id;
-            const buttonCollected = await interaction.channel.awaitMessageComponent({
-                filter: buttonFilter,
-                time: 60_000,
-            }).catch(() => null);
+                const followUp = await confirmation.reply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(Colors.Yellow)
+                            .setTitle('Logging in process.')
+                            .setDescription('The event has been submitted and is in the process of being logged. Please wait!')
+                            .setThumbnail('https://cdn.astrohweston.xyz/u/87efda8e-31df-424d-9881-efb7df7c996b.gif') // i know this is hardcoded, try me
+                            .setTimestamp()
+                            .setFooter({
+                                text: 'KOG Bot',
+                                iconURL: interaction.guild?.iconURL() as string
+                            })
+                    ],
+                    flags: MessageFlags.Ephemeral,
+                    withResponse: true
+                });
 
-            if (!buttonCollected) {
-                const timeoutEmbed = new EmbedBuilder()
-                    .setColor(Colors.Red)
-                    .setTitle('Error: Timeout')
-                    .setDescription('You took too long to respond. Please try again.')
-                    .setTimestamp();
+                await checkExistance(this.kogBot, loggedUsers);
 
-                await interaction.followUp({ embeds: [timeoutEmbed] });
-                return;
+                // Update events_attended for each attendee
+                for (const userId of loggedUsers) {
+                    await this.kogBot.database('users')
+                        .where({ discord_id: userId })
+                        .increment('events_attended', 1);
+                }
+
+                await logChannel.send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle('Event log')
+                            .setColor(Colors.Purple)
+                            .setFields(
+                                { name: 'Host', value: `<@${interaction.user.id}>`, inline: true },
+                                { name: 'Timestamp', value: `<t:${Math.floor(Date.now() / 1000)}:F>`, inline: true },
+                                { name: 'Attendees', value: loggedUsers.map(user => `- <@${user}>`).join(',\n') },
+                            )
+                            .setTimestamp()
+                            .setFooter({
+                                text: 'KOG Bot',
+                                iconURL: interaction.guild?.iconURL() as string
+                            })
+                    ]
+                });
+
+                await confirmation.message.edit({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle('Event logged.')
+                            .setDescription('The event has been successfully logged.')
+                            .setColor(Colors.Green)
+                            .setFields(
+                                { name: 'Host', value: `<@${interaction.user.id}>` },
+                                { name: 'Attendees', value: loggedUsers.map(user => `- <@${user}>`).join('\n') }
+                            )
+                            .setTimestamp()
+                            .setFooter({
+                                text: 'KOG Bot',
+                                iconURL: interaction.guild?.iconURL() as string
+                            })
+                    ],
+                    components: []
+                });
+
+                await confirmation.editReply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(Colors.Green)
+                            .setTitle('Event logged.')
+                            .setDescription('The event has been successfully logged.')
+                            .setThumbnail('https://cdn.astrohweston.xyz/u/0ab52b9e-646f-47df-8607-f2b68b0b6307.gif') // i know this is hardcoded too, try me
+                            .setTimestamp()
+                            .setFooter({
+                                text: 'KOG Bot',
+                                iconURL: interaction.guild?.iconURL() as string
+                            })
+                    ]
+                });
+                
+            } else if (confirmation?.customId === 'cancel_log') {
+                await confirmation.update({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle('Event log cancelled.')
+                            .setDescription('The event log has been cancelled.')
+                            .setColor(Colors.Red)
+                            .setTimestamp()
+                            .setFooter({
+                                text: 'KOG Bot',
+                                iconURL: interaction.guild?.iconURL() as string
+                            })
+                    ],
+                    components: []
+                });
             }
-
-            if (buttonCollected.customId === 'cancel') {
-                const cancelEmbed = new EmbedBuilder()
-                    .setColor(Colors.Red)
-                    .setTitle('Event Logging Canceled')
-                    .setDescription('You have canceled the event log.')
-                    .setTimestamp();
-
-                await buttonCollected.reply({ embeds: [cancelEmbed] });
-                return;
-            }
-
-            if (buttonCollected.customId === 'done') {
-    let databaseUpdated = false; // Placeholder for database interaction logic
-    const doneEmbed = new EmbedBuilder()
-        .setColor(databaseUpdated ? "#9033FF" : Colors.Red)
-        .setTitle('Event Logging Completed')
-        .addFields(
-            { name: 'Host', value: `<@${interaction.user.id}>`, inline: true },
-            { name: 'Timestamp', value: `<t:${Math.floor(Date.now() / 1000)}:F>`, inline: true },
-            { name: 'Attendees', value: mentions.join(', ') || 'None', inline: false },
-            { name: 'Squadron Rally', value: 'False', inline: false },
-            { name: 'Database Updated', value: databaseUpdated ? 'True' : 'Failed', inline: false }
-        )
-        .setTimestamp();
-
-    // Fetch the specific channel
-    const logChannel = interaction.client.channels.cache.get('1002610487378321520') as TextChannel; // i'll optimize this im just tired :(
-
-    if (logChannel) {
-        await logChannel.send({ embeds: [doneEmbed] });
-    } else {
-        console.error('Log channel not found!');
+        } catch (err) {
+            console.log(err);
+        }
     }
+}
 
-    const loggedEmbed = new EmbedBuilder()
-        .setColor(Colors.Green)
-        .setTitle('Logged Successfully')
-        .setDescription('The event has been logged successfully.')
-        .setTimestamp();
+async function checkExistance (kogBot: KOGBot, userIds: string[]) {
+    for (const userId of userIds) {
+        const userData = await kogBot.database('users')
+        .select('*')
+        .where('discord_id', userId)
+        .first();
 
-    await buttonCollected.reply({ embeds: [loggedEmbed] });
-            }
+        if (!userData) {
+            await kogBot.database('users')
+                .insert({ discord_id: userId });
         }
     }
 }
